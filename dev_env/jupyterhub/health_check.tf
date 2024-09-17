472c896e4349683cd991e1045cbe9214be4babe8
James McDuffie <James.McDuffie@jpl.nasa.gov>
2024-09-17 12:06:26 -0700

    Change health check URL to public address

--- dev_env/jupyterhub/health_check.tf
+++ dev_env/jupyterhub/health_check.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "jupyter_health_url" {
   name = "/unity/${var.project}/${var.venue}/component/jupyter"
   type = "String"
   value = jsonencode({
-    healthCheckUrl = "${module.frontend.internal_base_url}/${module.frontend.jupyter_base_path}/hub/health"
+    healthCheckUrl = "${module.frontend.jupyter_base_url}/${module.frontend.jupyter_base_path}/hub/health"
     landingPageUrl = "${module.frontend.jupyter_base_url}/${module.frontend.jupyter_base_path}/"
     componentName  = "Jupyterhub"
   })
