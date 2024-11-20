44f7f60f6d46b6db7cba90c6a37e65f930db891e
Gerald Manipon <pymonger@gmail.com>
2024-11-20 12:48:26 -0800

    add

---
+++ dev_env/shared_storage/variables.tf
@@ -0,0 +1,28 @@
+variable "project" {
+  description = "The name of the project matching the /unity/<{project>/<venue</project-name SSM parameter"
+  type        = string
+  default     = "unity"
+}
+
+variable "venue" {
+  description = "The name of the unity venue matching the /unity/<project>/<venue>/venue-name SSM parameter"
+  type        = string
+}
+
+variable "deployment_name" {
+  description = "Optional string to place before the venue name in resource names"
+  type        = string
+  default     = ""
+}
+
+variable "resource_prefix" {
+  description = "String used at the beginning of the names for all resources to identify them according to the UADS subsystem"
+  type        = string
+  default     = "uads"
+}
+
+variable "efs_identifier" {
+  description = "EFS file system to connect Jupyter shared storage with"
+  type        = string
+  # Example value:uads-development-efs-fs"
+}
