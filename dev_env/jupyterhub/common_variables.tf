3ac70bd324aa32d4dc74304e37ada374dac2936a
Gerald Manipon <pymonger@gmail.com>
2024-11-20 14:49:39 -0800

    undo symlinking

--- dev_env/jupyterhub/common_variables.tf
+++ dev_env/jupyterhub/common_variables.tf
@@ -1,1 +1,28 @@
-../../common/variables.tf
+variable "project" {
+  description = "The name of the project matching the /unity/<{project>/<venue</project-name SSM parameter"
+  type        = string
+  default     = "unity"
+}
+
+variable "venue" {
+  description = "The name of the unity venue matching the /unity/<project>/<venue>/venue-name SSM parameter"
+  type        = string
+}
+
+variable "deployment_name" {
+  description = "Optional string to place before the venue name in resource names"
+  type        = string
+  default     = ""
+}
+
+variable "resource_prefix" {
+  description = "String used at the beginning of the names for all resources to identify them according to the UADS subsystem"
+  type        = string
+  default     = "uads"
+}
+
+variable "efs_identifier" {
+  description = "EFS file system to connect Jupyter shared storage with"
+  type        = string
+  # Example value:uads-development-efs-fs"
+}
