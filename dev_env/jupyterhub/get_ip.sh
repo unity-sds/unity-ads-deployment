121ad64132af3669a74eee22ba3b2cab7661c07d
Gerald Manipon <pymonger@gmail.com>
2025-01-08 16:08:38 -0800

    handle deployment from non-AWS environments

--- dev_env/jupyterhub/get_ip.sh
+++ dev_env/jupyterhub/get_ip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-set -e
-IP=$(curl -s 'http://169.254.169.254/latest/meta-data/local-ipv4')
-jq -n --arg ip "$IP" '{"ip":$ip}'
+IP=$(curl --connect-timeout 1 -s 'http://169.254.169.254/latest/meta-data/local-ipv4')
+if [ "$IP" = "" ]; then
+  IP=127.0.0.1
+fi
+echo "{\"ip\": \"${IP}\"}"
